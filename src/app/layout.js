f9442779c971287509eb1b6e0040de1737c2e7ed
ScanAlign <atifjan2019@gmail.com>
2026-04-06 20:10:45 +0500

    feat: initialize root layout with Rubik font, metadata, and Smartlook integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,20 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${rubik.className} h-full antialiased`}
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.smartlook||(function(d) {
+              var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+              var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+              c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+              })(document);
+              smartlook('init', '8c98766d5ea81fbc70c12433b0f2cb67361ccd36', { region: 'eu' });
+            `
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col" cz-shortcut-listen="true">{children}</body>
     </html>
   );
